ef2a5c909f719d001ec2e36f85bd08ce8980305c
Robert Ward <robert@rtward.com>
2014-07-11 22:06:24 -0500

    Implemented a very simple command parser and console output

--- src/tweetmybot/CommandParser.py
+++ src/tweetmybot/CommandParser.py
@@ -1,11 +1,14 @@
 from threading import Thread
 import time
+from Queue import Empty
 
 
 class CommandParser(Thread):
     def __init__(self, command_queue, action_queue, feedback_queue):
         Thread.__init__(self)
         self.command_queue = command_queue
+        self.action_queue = action_queue
+        self.feedback_queue = feedback_queue
         self.running = True
 
     def stop(self):
@@ -13,5 +16,41 @@ class CommandParser(Thread):
 
     def run(self):
         while self.running:
+            try:
+                command = self.command_queue.get_nowait()
+
+                action = self.parse_command(command)
+
+                if action is not None:
+                    self.action_queue.put(action)
+                    self.feedback_queue.put(action)
+
+            except Empty:
+                pass
+
             time.sleep(0)
         print "Stopping command parser"
+
+    def parse_command(self, command_string):
+        command_string = command_string.upper()
+
+        command = None
+        direction = None
+        duration = 10
+
+        if "FORWARD" in command_string:
+            command = "move"
+            direction = 1
+        elif "BACKWARDS" in command_string:
+            command = "move"
+            direction = 2
+        elif "LEFT" in command_string:
+            command = "move"
+            direction = 3
+        elif "RIGHT" in command_string:
+            command = "move"
+            direction = 4
+
+        if command is not None:
+            self.action_queue.put((command, direction, duration))
+            self.feedback_queue.put((command, direction, duration))
--- src/tweetmybot/output/Console.py
+++ src/tweetmybot/output/Console.py
@@ -1,11 +1,12 @@
 from threading import Thread
 import time
+from Queue import Empty
 
 
 class Console(Thread):
-    def __init__(self, command_queue):
+    def __init__(self, action_queue):
         Thread.__init__(self)
-        self.command_queue = command_queue
+        self.action_queue = action_queue
         self.running = True
 
     def stop(self):
@@ -13,5 +14,14 @@ class Console(Thread):
 
     def run(self):
         while self.running:
+            try:
+                action = self.action_queue.get_nowait()
+
+                print action
+
+            except Empty:
+                pass
+
             time.sleep(0)
+
         print "Stopping Console Output"
